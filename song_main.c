6966e83dc14683b4f38945de050aca2e4c03c0cc
jgrowden <139929074+jgrowden@users.noreply.github.com>
2023-07-31 11:33:11 +1000

    Update song_main.c

--- song_main.c
+++ song_main.c
@@ -5,7 +5,7 @@ extern struct tone off_sound[];
 extern struct tone on_sound[]; 
 extern int song_duration;
 extern void udelay(unsigned int delay_in_us);
-extern void sound(unsigned int duration, int period,  int vol);
+extern void play_tone(unsigned int duration, int period,  int vol);
 extern void setup_DAC(void);
 extern int startup; // representing on/off state, determines when 'sound' is called
                     // assume that when system is turning off, startup is set to low
@@ -20,12 +20,12 @@ setup_DAC();
 
 if (startup == 0) {
     for (i = 0; i < song_duration; i++) {
-        sound(rate * on_sound[i].duration, on_sound[i].pitch, on_sound[i].volume);
+        play_tone(rate * on_sound[i].duration, on_sound[i].pitch, on_sound[i].volume);
     }
 }
 else if (startup == 1) {
     for (i = 0; i < song_duration; i++) {
-        sound(rate * off_sound[i].duration, off_sound[i].pitch, off_sound[i].volume);  
+        play_tone(rate * off_sound[i].duration, off_sound[i].pitch, off_sound[i].volume);  
     }
 }
 
