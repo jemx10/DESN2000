8d984d2bd22528140b8d79b1760d4de37f6f1e3b
jgrowden <139929074+jgrowden@users.noreply.github.com>
2023-07-31 11:40:36 +1000

    Update song_main.c

--- song_main.c
+++ song_main.c
@@ -7,9 +7,7 @@ extern int song_duration;
 extern void udelay(unsigned int delay_in_us);
 extern void play_tone(unsigned int duration, int period,  int vol);
 extern void setup_DAC(void);
-extern int startup; // representing on/off state, determines when 'sound' is called
-                    // assume that when system is turning off, startup is set to low
-                    // when turning on, startup set to high, will need to be accounted for in main
+extern struct current current;
 
 int song_rate = (TBC);            //will play around with different values
 
@@ -18,12 +16,12 @@ int i;
 
 setup_DAC();
 
-if (startup == 0) {
+if (current->on_off == 0) {
     for (i = 0; i < song_duration; i++) {
         play_tone(rate * on_sound[i].duration, on_sound[i].pitch, on_sound[i].volume);
     }
 }
-else if (startup == 1) {
+else if (current->on_off == 1) {
     for (i = 0; i < song_duration; i++) {
         play_tone(rate * off_sound[i].duration, off_sound[i].pitch, off_sound[i].volume);  
     }
