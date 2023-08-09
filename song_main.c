c04f831f151e4bca0f8ddd6669512187cb9df9c9
jgrowden <139929074+jgrowden@users.noreply.github.com>
2023-08-09 11:03:16 +1000

    Update song_main.c

--- song_main.c
+++ song_main.c
@@ -1,31 +1,30 @@
 #include "lpc24xx.h"
 #include "songs.h"
 
-extern struct tone off_sound[]; 
-extern struct tone on_sound[]; 
-extern int song_duration;
-extern void udelay(unsigned int delay_in_us);
-extern void play_tone(unsigned int duration, int period,  int vol);
-extern void setup_DAC(void);
-extern struct current current;
+struct tone off_sound[]; 
+struct tone on_sound[]; 
+int song_duration;
+void udelay(unsigned int delay_in_us);
+void play_tone(unsigned int duration, int period,  int vol);
+void setup_DAC(void);
+int main() {
 
-//int rate = (TBC);            //will play around with different values
+    //int rate = ;            //experiment with different values
 
+    int i;
 
-int i;
+    setup_DAC();
 
-setup_DAC();
+    //not sure how to make this edge triggered at the on/off signal
 
-//not sure how to make this edge triggered at the on/off signal
-
-if (current->on_off == 0) {
-    for (i = 0; i < song_duration; i++) {
-        play_tone(rate * on_sound[i].duration, on_sound[i].pitch, on_sound[i].volume);
+    if (current->on_off == 0) {
+        for (i = 0; i < song_duration; i++) {
+            play_tone(rate * on_sound[i].duration, on_sound[i].pitch, on_sound[i].volume);
+        }
     }
-}
-else if (current->on_off == 1) {
-    for (i = 0; i < song_duration; i++) {
-        play_tone(rate * off_sound[i].duration, off_sound[i].pitch, off_sound[i].volume);  
+    else if (current->on_off == 1) {
+        for (i = 0; i < song_duration; i++) {
+            play_tone(rate * off_sound[i].duration, off_sound[i].pitch, off_sound[i].volume);  
+        }
     }
-}
 
